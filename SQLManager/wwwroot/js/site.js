c7399333c537a7f28a052cb036c6468556c66ae8
mitsoshxc <mitsoshxc@gmail.com>
2018-03-06 00:00:36 +0200

    Initial commit

---
+++ SQLManager/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿$("#SQLConnection").hide();
+//
+// SQL type selection
+//
+function SelectSQL() {
+    var _Selection = $("#SelectSQL input[type='radio']:checked").val();
+
+    $("#SQLType").hide();
+
+    if (_Selection == "SQLServer") {
+        $("#SQLServer").removeClass("hidden");
+        $("#SQLServer").show();
+    }
+    else if (_Selection == "MySQL") {
+        $("#MySQL").removeClass("hidden");
+        $("#MySQL").show();
+    }
+    else {
+        $("#SQLite").removeClass("hidden");
+        $("#SQLite").show();
+    }
+
+    return false;
+}
