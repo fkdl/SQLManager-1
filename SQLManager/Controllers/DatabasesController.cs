e49f23781686990f1195b385f6909ccfd4c25cae
mitsoshxc <mitsoshxc@gmail.com>
2018-04-23 15:41:18 +0300

    MySQL connection & list MySQL databases

--- SQLManager/Controllers/DatabasesController.cs
+++ SQLManager/Controllers/DatabasesController.cs
@@ -13,24 +13,53 @@ namespace SQLManager.Controllers
         public async Task<IActionResult> Index()
         {
             ViewBag.Title = "All Databases";
-            using (var _conn = new SqlConnection(Extensions.Connection[1]))
+            if (Extensions.Connection[0].Equals("SQLServer"))
             {
-                await _conn.OpenAsync();
-                using (var _Command = _conn.CreateCommand())
+                using (var _conn = new SqlConnection(Extensions.Connection[1]))
                 {
-                    _Command.CommandText = @"SELECT name FROM master.dbo.sysdatabases";
-                    using (var _reader = await _Command.ExecuteReaderAsync())
+                    await _conn.OpenAsync();
+                    using (var _Command = _conn.CreateCommand())
                     {
-                        var _databases = new List<string>();
-
-                        while (await _reader.ReadAsync())
+                        _Command.CommandText = @"SELECT name FROM master.dbo.sysdatabases";
+                        using (var _reader = await _Command.ExecuteReaderAsync())
                         {
-                            _databases.Add(_reader.GetString(0));
-                        }
+                            var _databases = new List<string>();
 
-                        return View(_databases);
+                            while (await _reader.ReadAsync())
+                            {
+                                _databases.Add(_reader.GetString(0));
+                            }
+
+                            return View(_databases);
+                        }
                     }
                 }
+            }
+            else if (Extensions.Connection[0].Equals("MySQL"))
+            {
+                using (var _conn = new MySqlConnection(Extensions.Connection[1]))
+                {
+                    await _conn.OpenAsync();
+                    using (var _Command = _conn.CreateCommand())
+                    {
+                        _Command.CommandText = @"SHOW databases";
+                        using (var _reader = await _Command.ExecuteReaderAsync())
+                        {
+                            var _databases = new List<string>();
+
+                            while (await _reader.ReadAsync())
+                            {
+                                _databases.Add(_reader.GetString(0));
+                            }
+
+                            return View(_databases);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
             }
         }
     }
--- SQLManager/Controllers/HomeController.cs
+++ SQLManager/Controllers/HomeController.cs
@@ -78,6 +78,58 @@ namespace SQLManager.Controllers
             }
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MySQLContext(string _server, string _db, string _user, string _pass)
+        {
+            try
+            {
+                var _connection = new MySqlConnectionStringBuilder();
+
+                var _sp = _server.Split(':');
+
+                _connection.Server = _sp[0];
+                if (_sp.Length > 1)
+                {
+                    _connection.Port = uint.Parse(_sp[1]);
+                }
+                else
+                {
+                    _connection.Port = 3306;
+                }
+                _connection.UserID = _user;
+                _connection.Password = _pass;
+                
+                if (_db != null)
+                {
+                    _connection.Database = _db;
+                }
+
+                using (var _conn = new MySqlConnection(_connection.ToString()))
+                {
+                    await _conn.OpenAsync();
+                }
+
+                Extensions.Connection[0] = "MySQL";
+                Extensions.Connection[1] = _connection.ToString();
+                var _t = _connection.ToString();
+
+                if (_db != null)
+                {
+                    return RedirectToAction("Index", "Database", new { Name = _db });
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Databases");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
